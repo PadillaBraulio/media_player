b51a89562575199ee01a8a5766fa92d480d77a0a
Braulio Padilla <braulio@nclouds.com>
2019-11-20 21:01:33 -0600

    Adding initial layout

---
+++ media/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Video from 'react-native-video';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+  const onBuffer = onBuffer => {
+
+  };
+
+  const videoError = onBuffer => {
+
+  };
+
+  const var1 = "";
+
+  return (
+
+    <View style={{flex: 1}}>
+      <Video
+        style={styles.backgroundVideo}
+        source={{ uri: 'https://trambo-video.s3.amazonaws.com/video.mp4' }}
+        selectedVideoTrack={{
+          type: "resolution",
+          value: 1080
+        }}
+      />
+    </View>
+
+  );
+}
+
+var styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  screen: {
+      padding: 50
+    },
+});
